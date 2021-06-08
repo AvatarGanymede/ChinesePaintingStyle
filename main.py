f795f8e41f30dbaf38aae491f0d30f8ef483d35a
AvatarGanymede <liu.ruitong@outlook.com>
2021-06-08 18:46:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow_examples.models.pix2pix import pix2pix
+
+import os
+import time
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+tfds.disable_progress_bar()
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+dataset, metadata = tfds.load('cycle_gan/horse2zebra',
+                              with_info=True, as_supervised=True)
+
+train_horses, train_zebras = dataset['trainA'], dataset['trainB']
+test_horses, test_zebras = dataset['testA'], dataset['testB']
+
